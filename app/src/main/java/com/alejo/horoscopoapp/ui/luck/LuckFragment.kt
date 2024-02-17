84868f70b388a714d235b5fb170838b6146e26b8
alejo <alejomarti95@gmail.com>
2024-02-16 19:30:47 -0500

    First commit - project 2

---
+++ app/src/main/java/com/alejo/horoscopoapp/ui/luck/LuckFragment.kt
@@ -0,0 +1,23 @@
+package com.alejo.horoscopoapp.ui.luck
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.alejo.horoscopoapp.R
+import com.alejo.horoscopoapp.databinding.FragmentHoroscopeBinding
+import com.alejo.horoscopoapp.databinding.FragmentLuckBinding
+
+class LuckFragment : Fragment() {
+    private var _binding: FragmentLuckBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentLuckBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+}
