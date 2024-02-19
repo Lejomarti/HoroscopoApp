38b3f77e1ecbb93af1b208fdb8909535297f27af
alejo <alejomarti95@gmail.com>
2024-02-19 16:32:39 -0500

    DaggerHilt configured in the Fragments. A bug was fixed. Some design added

---
+++ app/src/main/java/com/alejo/horoscopoapp/HoroscopoApp.kt
@@ -0,0 +1,9 @@
+package com.alejo.horoscopoapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class HoroscopoApp:Application() {
+}
--- app/src/main/java/com/alejo/horoscopoapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/alejo/horoscopoapp/ui/palmistry/PalmistryFragment.kt
@@ -7,13 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.alejo.horoscopoapp.R
 import com.alejo.horoscopoapp.databinding.FragmentPalmistryBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class PalmistryFragment : Fragment() {
 
     private var _binding: FragmentPalmistryBinding? = null
     private val binding get() = _binding!!
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
